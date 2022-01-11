9d9a31688cf9f0c9ec0caac63a6b0952d91d1689
EvincarSN <31027616+EvincarSN@users.noreply.github.com>
2022-01-11 17:27:44 -0300

    Update index.php
    CLI access only

--- index.php
+++ index.php
@@ -1,29 +1,3 @@
 <?php
-// require __DIR__."/vendor/autoload.php";
 
-// use ASPTest\Controllers\User;
-
-// $data = [
-//     "userFirstName" => "aa",
-//     "userLastName" => "ba",
-//     "userEmail" => "aa@gmail.com",
-//     "userAge" => 149
-// ];
-
-// $user = new User();  
-// $results = $user->validateCreateUser($data);  
-
-// if(array_key_exists('status', $results)) {
-//     $status = $results['status'];
-// }
-
-
-// echo '</pre>';
-// print_r($data);
-// print_r($results);
-// die();
-
-
-
-
-
+die("CLI access only");
